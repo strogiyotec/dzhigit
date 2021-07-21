ce42e4031064e42a2e1ed626da227856dced1c40
strogiyotec <almas337519@gmail.com>
2021-07-20 19:03:28 -0700

    finish git formatter read object and add test for header and serialize

--- repository/git_object.go
+++ repository/git_object.go
@@ -4,22 +4,84 @@ import (
 	"bytes"
 	"compress/zlib"
 	"crypto/sha1"
+	"encoding/binary"
+	"errors"
 	"fmt"
+	"strings"
 )
 
+type GitObjectType string
+
+const (
+	BLOB GitObjectType = "blob"
+	TREE               = "tree"
+)
+
+//convert given string to git object type
+func asGitObjectType(str string) (GitObjectType, error) {
+	switch str {
+	case "blob":
+		return BLOB, nil
+	case "tree":
+		return TREE, nil
+	default:
+		return "",
+			errors.New(
+				fmt.Sprintf(
+					"%s is not a valid git object type",
+					str,
+				),
+			)
+	}
+}
+
 type GitFileFormatter interface {
-	Serialize(data []byte, objType string) (*GitObject, error)
+	Serialize(data []byte, objType string) (*SerializedGitObject, error)
+	Deserialize(data []byte) (*DeserializedGitObject, error)
 }
 
 type DefaultGitFileFormatter struct {
 }
 
-type GitObject struct {
+type DeserializedGitObject struct {
+	objType GitObjectType
+	content []byte
+}
+
+func NewDeserializedGitObject(objType string, cotent []byte) {
+}
+
+type SerializedGitObject struct {
 	hash    []byte
 	content []byte
 }
 
-func (obj *DefaultGitFileFormatter) Serialize(data []byte, objType string) (*GitObject, error) {
+func (obj *DefaultGitFileFormatter) Deserialize(data []byte) (*DeserializedGitObject, error) {
+	spaceIndex := strings.Index(string(data), " ")
+	objType, err := asGitObjectType(string(data[0:spaceIndex]))
+	if err != nil {
+		return nil, err
+	}
+	nullIndex := strings.Index(string(data), "\x00")
+	length := binary.BigEndian.Uint32(data[spaceIndex:nullIndex])
+	if length != uint32(len(data)-nullIndex-1) {
+		return nil,
+			errors.New(
+				fmt.Sprintf(
+					`Invalid git object for deserialization , 
+                    the length in the head is %d , expected %d\n`,
+					length,
+					len(data)-nullIndex-1,
+				),
+			)
+	}
+	return &DeserializedGitObject{
+		objType: objType,
+		content: data[nullIndex+1:],
+	}, nil
+}
+
+func (obj *DefaultGitFileFormatter) Serialize(data []byte, objType GitObjectType) (*SerializedGitObject, error) {
 	header := header(data, objType)
 	result := append(header, data...)
 	hash, err := hashed(result)
@@ -30,7 +92,7 @@ func (obj *DefaultGitFileFormatter) Serialize(data []byte, objType string) (*Git
 	if err != nil {
 		return nil, err
 	}
-	return &GitObject{
+	return &SerializedGitObject{
 		hash:    hash,
 		content: zipped,
 	}, nil
@@ -58,10 +120,10 @@ func hashed(data []byte) ([]byte, error) {
 
 }
 
-func header(data []byte, objType string) []byte {
+func header(data []byte, objType GitObjectType) []byte {
 	return []byte(
 		fmt.Sprintf(
-			"%s %d \x00",
+			"%s %d\x00",
 			objType,
 			len(data),
 		),
---
+++ repository/git_object_test.go
@@ -0,0 +1,30 @@
+package repository
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"testing"
+)
+
+func Test_SerializeAndDeserialize(t *testing.T) {
+	fileFormatter := DefaultGitFileFormatter{}
+	serialized, err := fileFormatter.Serialize([]byte("Hello world"), BLOB)
+	if err != nil {
+		t.Fatal(err.Error())
+		return
+	}
+	if len(serialized.hash) != sha1.Size {
+		fmt.Println("Invalid hash length, has to be 40 ")
+	}
+	//TODO: test content of the serialized object
+}
+
+func Test_header(t *testing.T) {
+	data := []byte("Hello world")
+	header := header(data, BLOB)
+	if string(header) != "blob 11\x00" {
+		t.Fatal(
+			"Invalid header",
+		)
+	}
+}
